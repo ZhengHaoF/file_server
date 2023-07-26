4a85598dae6549d2daa0be32ae081c3a2a0cf938
zhf8 <1715005995@qq.com>
2023-07-26 09:43:08 +0800

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# 简易文件服务器后台
+
+### 使用方法
+1. 修改config.json中需要共享的目录
+2. 使用 `node index.js` 运行
+3. 启动浏览器，打开`localhost:3000`即可访问
+
+### 在线预览
+支持常见图片，视频格式预览
+
+### 前端地址
+https://github.com/ZhengHaoF/file_server_web
---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const cors = require('cors')
+const fs = require('fs');
+const path = require('path')
+const app = express()
+const port = 3000
+const rootPath = require("./config.json")['rootPath'];
+// const mime = require("./mime.json");
+app.use(cors())
+app.use(express.static('web'))
+//托管静态文件
+app.use('/getFile', express.static(rootPath))
+
+//获取当前文件夹
+function getNowPath(filePath) {
+    let nowPath = "";
+    if (filePath === "$") {
+        filePath = ""
+    } else {
+        filePath = filePath.replace(/\$/g, "")
+    }
+    filePath = filePath.replace(/__/g, "/")
+    nowPath = rootPath + filePath;
+    console.log(nowPath)
+    return nowPath;
+}
+
+//获取文件列表
+app.get('/list/:filePath', (req, res) => {
+    let nowPath = getNowPath(req.params.filePath)
+    fs.readdir(nowPath, (err, files) => {
+        if (err) {
+            throw err;
+        }
+        let data = [];
+        files.forEach(fileName => {
+            let fileInfo = fs.statSync(nowPath + '/' + fileName);
+            data.push({
+                "name": fileName,
+                "size": fileInfo.size,
+                "isDirectory": fileInfo.isDirectory(),
+                "isFile": fileInfo.isFile(),
+                "suffix": path.extname(fileName),
+            })
+        });
+        return res.json(data);
+    });
+
+})
+app.listen(port, () => {
+    console.log("启动服务")
+    console.log(`监听端口 ${port}`)
+})
